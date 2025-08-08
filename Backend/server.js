0c49134baaed20adfe2774f7f31f4e280393d005
Mohamed Ayari <mohamed.ayarii1507@gmail.com>
2025-08-08 16:19:58 +0100

    add cors

--- Backend/server.js
+++ Backend/server.js
@@ -1,8 +1,10 @@
 const express = require('express')
-
 const app = express()
 
 app.use(express.json())
+
+const cors = require('cors')
+app.use(cors())
 
 require('./config/connect')
 
@@ -12,7 +14,7 @@ require('dotenv').config();
 const userRouter = require('./routes/user')
 app.use('/userRouter', userRouter)
 
-app.use('/getImages', express.static('./uploads'))
+app.use('/getImages', express.static('uploads'))
 
 
 app.listen('3000', () => {
