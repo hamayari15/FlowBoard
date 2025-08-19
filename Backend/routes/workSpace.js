04c62487b76f5f80da80f2ad2bf842d14473797a
Mohamed Ayari <mohamed.ayarii1507@gmail.com>
2025-08-19 20:10:49 +0100

    getById, update && delete workSpace endpoints

--- Backend/routes/workSpace.js
+++ Backend/routes/workSpace.js
@@ -1,12 +1,18 @@
-const express = require('express')
-const Router = express.Router()
+const express = require('express');
+const Router = express.Router();
 
-const workSpaceController = require('../controllers/workSpace.controller')
+const workSpaceController = require('../controllers/workSpace.controller');
 
 
 Router.post('/Add', workSpaceController.Add)
 
 Router.get('/getAll', workSpaceController.getAll)
 
+Router.get('/getById/:id', workSpaceController.getById)
 
-module.exports = Router
+Router.put('/Update/:id', workSpaceController.Update)
+
+Router.delete('/Delete/:id', workSpaceController.Delete)
+
+
+module.exports = Router;
--- Frontend/src/app/core/services/workspace.service.ts
+++ Frontend/src/app/core/services/workspace.service.ts
@@ -12,10 +12,22 @@ export class WorkspaceService {
 
   addWorkSpace(workSpaceData: any) {
     return this.http.post(`${this.apiUrl}/Add`, workSpaceData)
-  };
+  }
 
   getworkSpaces() {
     return this.http.get(`${this.apiUrl}/getAll`)
-  };
+  }
+
+  getWorkSpaceById(id: any) {
+    return this.http.get(`${this.apiUrl}/getById/${id}`)
+  }
+
+  updateWoksSpace(newData: any, id: any) {
+    return this.http.put(`${this.apiUrl}/Update/${id}`, newData)
+  }
+
+  deleteWorkSpace(id: any) {
+    return this.http.delete(`${this.apiUrl}/Delete/${id}`)
+  }
 
 };
