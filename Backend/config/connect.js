2ba67279f422ea965852385384a5ba95593e5fdd
amine-a11 <mohamedaminehajji11@gmail.com>
2025-11-09 10:22:57 +0100

    create and add remote mongo cluster

--- Backend/config/connect.js
+++ Backend/config/connect.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 
-mongoose.connect('mongodb://127.0.0.1:27017/flowBoard')
+mongoose.connect(process.env.MONGODB_URI)
     .then(
         () => {
             console.log("Connected to DB");
